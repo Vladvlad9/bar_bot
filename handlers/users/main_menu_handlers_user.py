5ded77f16e9e5f4ca0e42daef01d7d761ca24722
Vlad Paulechka <vlad9.ru@gmail.com>
2022-03-11 21:43:53 +0300

    create new function

--- handlers/users/main_menu_handlers_user.py
+++ handlers/users/main_menu_handlers_user.py
@@ -4,16 +4,26 @@ from keyboards import *
 from loader import dp, db
 
 
-@dp.message_handler(text="Основное меню")
+@dp.message_handler(text=["Основное меню", "Барное меню", "Пивная карта", "Пиво в банке"])
 async def load_name(message: types.Message):
+    data = {
+        'Основное меню': 'main_menu',
+        'Барное меню': 'bar_menu',
+        'Пивная карта': 'beer_card',
+        'Пиво в банке': 'beer_in_can'
+    }
+
     await message.answer("Наше меню", reply_markup= await back_kb())
 
     markup = types.InlineKeyboardMarkup()
     types.InlineKeyboardMarkup()
 
-    photo = await db.get_photo_main_menu()
-    for photo in photo:
-        await message.answer_photo(photo[0], reply_markup=markup)
+    for i, j in data.items():
+        if message.text in i:
+            photo = await db.get_photo_main_menu(j)
+            for photo in photo:
+                await message.answer_photo(photo[0], reply_markup=markup)
+                break
 
 
 @dp.message_handler(text="Назад")
