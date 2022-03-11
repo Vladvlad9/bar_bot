0b3805c74b8604233840a7fbe33c2a1760c9bbf0
Vlad Paulechka <vlad9.ru@gmail.com>
2022-03-11 21:42:59 +0300

    add import

--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -1,7 +1,9 @@
 from .main_menu_handlers_user import dp
+from .reviews import dp
 from .help import dp
 from .start import dp
 from .echo import dp
+
 
 
 __all__ = ["dp"]
--- states/__init__.py
+++ states/__init__.py
@@ -1,1 +1,2 @@
 from .photo_states import *
+from .review import *
