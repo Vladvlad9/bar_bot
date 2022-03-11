17745ed72e9bad8b897221fb2f4460cb995f216d
Vlad Paulechka <vlad9.ru@gmail.com>
2022-03-11 17:46:42 +0300

    create new function start_kb_admin

---
+++ keyboards/default/admin/adminKB.py
@@ -0,0 +1,21 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
+
+
+
+async def start_kb_admin():
+    keyboard = ReplyKeyboardMarkup(
+        row_width=2,
+        resize_keyboard=True,
+        one_time_keyboard=False,
+        keyboard=[
+            [
+                KeyboardButton(text='Основное меню_admin'),
+                KeyboardButton(text='Барное меню_admin')
+            ],
+            [
+                KeyboardButton(text='Пивная карта_admin'),
+                KeyboardButton(text='Пиво в банке_admin')
+            ]
+        ]
+    )
+    return keyboard
