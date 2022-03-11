e0d3fef8bf526c2df73bdd534ac8e11140c4d20a
Vlad Paulechka <vlad9.ru@gmail.com>
2022-03-11 17:43:21 +0300

    add functions 'moderator and create db'

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -2,9 +2,20 @@ from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
 
 from keyboards.default import start_kb
-from loader import dp
+from keyboards.default.admin import adminKB
+from loader import dp, db
 
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
     await message.answer(f'Привет, {message.from_user.full_name}!', reply_markup= await start_kb())
+
+
+@dp.message_handler(commands=["create_database", "create_db", ])
+async def create_database(message: types.Message):
+    await message.answer(text="База данных успешно создана!")
+    await db.create_all_database()
+
+@dp.message_handler(commands=["moderator", "admin", ])
+async def create_database(message: types.Message):
+    await message.answer(text="Вы вошли как админ", reply_markup=await adminKB.start_kb_admin())
