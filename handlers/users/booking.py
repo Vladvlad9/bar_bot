c66b6043dc55865b7fa3c9e905d7f63ffda19b55
Vlad Paulechka <vlad9.ru@gmail.com>
2022-03-13 02:07:26 +0300

    create functions cancel

--- handlers/users/booking.py
+++ handlers/users/booking.py
@@ -5,6 +5,7 @@ import aiogram_calendar.dialog_calendar
 
 from aiogram import types
 from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Text
 from aiogram.types import CallbackQuery
 from aiogram_calendar import simple_cal_callback, SimpleCalendar, dialog_cal_callback, DialogCalendar
 from keyboards import *
@@ -12,9 +13,19 @@ from loader import dp, db
 from states import bookingST
 
 
+@dp.message_handler(state='*', commands="Назад")
+@dp.message_handler(Text(equals='Назад', ignore_case=True), state='*')
+async def cancel_handler(message: types.Message, state: FSMContext):
+    current_state = await state.get_data()
+
+    if current_state is None:
+        return
+    await state.finish()
+
+
 @dp.message_handler(text=["Забронировать столик"])
 async def load_name(message: types.Message):
-    await message.answer("Введите Ваше имя", reply_markup=await back_kb())
+    await message.answer("Введите Ваше имя", reply_markup=await back_kb_mn())
     await bookingST.fname_user.set()
 
 
@@ -38,10 +49,12 @@ async def fname_user_stock(message: types.Message, state: FSMContext):
 async def process_simple_calendar(callback_query: CallbackQuery, callback_data: dict, state: FSMContext):
     selected, date = await SimpleCalendar().process_selection(callback_query, callback_data)
     if selected:
-        select_date_user = await callback_query.message.answer(
+        await callback_query.message.answer(
             f'Вы выбрали: {date.strftime("%d/%m/%Y")}'
+
         )
-    await state.update_data(dateST=select_date_user)
+    result_select_date_user = date.strftime("%d/%m/%Y")
+    await state.update_data(dateST=result_select_date_user)
     await bookingST.next()
     await bookingST.time.set()
     await callback_query.message.answer('Введите время посещения')
@@ -54,7 +67,7 @@ def is_valid_time(time) -> bool:
 @dp.message_handler(state=bookingST.time)
 async def timeST_stock(message: types.Message, state: FSMContext):
     await state.update_data(timeST=message.text)
-
+    await message.answer(f'Выберите время: ')
     #key = True
     # while key:
     #     if is_valid_time(message.text):
@@ -72,8 +85,21 @@ async def timeST_stock(message: types.Message, state: FSMContext):
 async def number_people_stock(message: types.Message, state: FSMContext):
     await state.update_data(number_people=message.text)
 
+    data_booking = await state.get_data()
+    lname = data_booking['lname']
+    fname = data_booking['fname']
+    date_booking = data_booking['dateST']
+    time_booking = data_booking['timeST']
+    number_people = data_booking['number_people']
+
+    if await db.add_booking_user(message.from_user.id, lname, fname, date_booking, time_booking, number_people):
+        await message.answer(f'{fname} вы успешно забронировали столик')
 
     await state.finish()
+
+
+
+
 
 
 
