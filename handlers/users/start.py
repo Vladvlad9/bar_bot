16c8fec8bbad4d6fac495036e10ba8aa69c73791
Vlad Paulechka <vlad9.ru@gmail.com>
2022-03-16 19:16:38 +0300

    add referral

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -3,12 +3,29 @@ from aiogram.dispatcher.filters.builtin import CommandStart
 
 from keyboards.default import start_kb
 from keyboards.default.admin import adminKB
-from loader import dp, db
+from loader import dp, db, bot
 
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
     await message.answer(f'Привет, {message.from_user.full_name}!', reply_markup= await start_kb())
+    referral = message.get_args()
+
+
+    if referral:
+        balance = await db.check_balance(referral)
+        new_balance = balance[0]
+        result_balance = new_balance[0] + 5
+        await db.add_new_user_referral(message.from_user.id, message.from_user.first_name, message.from_user.last_name,
+                                       referral)
+        await db.add_money_user(result_balance, referral)
+
+    if not await db.select_user(message.from_user.id):
+        await db.add_new_user(message.from_user.id, message.from_user.first_name, message.from_user.last_name)
+
+
+
+    print('asd')
 
 
 @dp.message_handler(commands=["create_database", "create_db", ])
