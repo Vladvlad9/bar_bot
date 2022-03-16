e863da5f8518aa0ae87915371c4fb09e392c9e7c
Vlad Paulechka <vlad9.ru@gmail.com>
2022-03-16 19:16:48 +0300

    new kb

--- keyboards/default/user/startKB.py
+++ keyboards/default/user/startKB.py
@@ -53,3 +53,30 @@ async def start_kb() -> ReplyKeyboardMarkup:
         ]
     )
     return keyboard
+
+async def start_kb() -> ReplyKeyboardMarkup:
+    keyboard = ReplyKeyboardMarkup(
+        row_width=3,
+        resize_keyboard=True,
+        one_time_keyboard=False,
+        keyboard=[
+            [
+                KeyboardButton(text="Меню")
+            ],
+            [
+
+                KeyboardButton(text="Футбольные трансляции"),
+                KeyboardButton(text="Акции"),
+                KeyboardButton(text="Забронировать столик"),
+            ],
+            [
+                KeyboardButton(text="Контакты"),
+                KeyboardButton(text="Оставить отзыв"),
+                KeyboardButton(text="Программа лояльности")
+            ],
+            [
+                KeyboardButton(text="Наши заведения")
+            ]
+        ]
+    )
+    return keyboard
